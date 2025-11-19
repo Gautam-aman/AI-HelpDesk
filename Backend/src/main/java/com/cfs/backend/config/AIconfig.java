febe4a760ed1c96287fc47d33ffd02df00682f10
Aman Gautam <amangautam2128@gmail.com>
2025-11-19 21:14:45 +0530

    Added jdbc support for message storage

--- Backend/src/main/java/com/cfs/backend/config/AIconfig.java
+++ Backend/src/main/java/com/cfs/backend/config/AIconfig.java
@@ -7,6 +7,8 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,7 +19,13 @@ public class AIconfig {
     Logger logger = org.slf4j.LoggerFactory.getLogger(this.getClass());
 
     @Bean
-    public ChatClient chatClient(ChatClient.Builder chatClientBuilder , ChatMemory chatMemory){
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder , JdbcChatMemoryRepository jdbcChatMemoryRepository) {
+
+        var chatMemory =  MessageWindowChatMemory.builder()
+                .chatMemoryRepository(jdbcChatMemoryRepository)
+                .maxMessages(20)
+                .build();
+
         logger.info(chatClientBuilder.toString());
         logger.info(chatMemory.getClass().getName());
         return chatClientBuilder.
