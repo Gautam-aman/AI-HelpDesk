6193732966f88c503664d6c222a45cc87bb0d2d0
Aman Gautam <amangautam2128@gmail.com>
2025-11-20 10:40:12 +0530

    Future scope

--- Backend/src/main/java/com/cfs/backend/TicketDatabaseTool/EmailTool.java
+++ Backend/src/main/java/com/cfs/backend/TicketDatabaseTool/EmailTool.java
@@ -10,6 +10,9 @@ public class EmailTool {
     // Send email to support team regarding new tickets
     @Tool(description = "this tool help in sending email to support team")
     public void sendEmail (@ToolParam(description = "Help in sending email to support team") String email , @ToolParam(description = "Short desc of issue") String message){
+        System.out.println("going to send email to support team");
+        System.out.println("email id : "+email);
+        System.out.println("message : "+message);
 
     }
 
